ee347171cc4a3f9aae2019cd0c2be75eef4ee175
Nicholas Yianilos <spartandeveloper@gmail.com>
2011-09-30 03:36:45 -0400

    Created a new rails app, modified gitignore and gemfile, installed rspec-rails.

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3', '1.3.4'
+gem 'rake', '0.8.7'
+
+group :development, :test do
+  gem 'rspec-rails', '2.6.1'
+end
+
+group :assets do
+  gem 'sass-rails', '3.1.3'
+  gem 'coffee-rails', '3.1.1'
+  gem 'uglifier', '1.0.3'
+end
+
+gem 'jquery-rails', '1.0.14'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HereSpy::Application.config.secret_token = '5820ba385b6f557da9c808b4b128a05540a49863f9c737d7e0252d30178fc0aad80e6049e3dec257a2e11c390db0c052c71c8ec38c9a72b9e41e26f214def7a7'
